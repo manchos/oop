0b907153ff213d875e379d0a83feefce3d3ca587
manchos <vladm.ru@gmail.com>
2016-06-24 17:43:51 +0400

    44

--- socnetwork.py
+++ socnetwork.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#https://checkio.org/mission/find-friends/share/3061fec1b35588e4bae8ad69db073aab/
 
 network = ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2", "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super")
 
